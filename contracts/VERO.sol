3aab58fc4b2b57c723a27d836324c044107d90a4
jcora-nyt <joseph.cora@nytimes.com>
2021-08-07 16:30:26 -0400

    Generic NFT (ERC721) smart contract for a VERO

--- contracts/VERO.sol
+++ contracts/VERO.sol
@@ -2,31 +2,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Import Auth from the access-control subdirectory
-import "./access-control/Auth.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract VERO {
+contract VERO is ERC721 {
     uint256 private _value;
-    Auth private _auth;
 
-    // Emitted when the stored value changes
-    event ValueChanged(uint256 value);
-
-    constructor(Auth auth) {
-        _auth = auth;
-    }
-
-    // Stores a new value in the contract
-    function store(uint256 value) public {
-        // Require that the caller is registered as an administrator in Auth
-        require(_auth.isAdministrator(msg.sender), "Unauthorized");
-
-        _value = value;
-        emit ValueChanged(value);
-    }
-
-    // Reads the last stored value
-    function retrieve() public view returns (uint256) {
-        return _value;
+    constructor() ERC721("VERO NFT", "ʍNFT") {
     }
 }
