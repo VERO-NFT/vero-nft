96e85f17ac52f37550e861220873ad2f37bf0c45
jcora-nyt <joseph.cora@nytimes.com>
2021-08-15 15:44:39 -0400

    Added steps to deploy contract to Ropsten test network

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,23 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
+const secrets = JSON.parse(fs.readFileSync(".secrets.json").toString().trim());
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "5777",
+    },
+    ropsten: {
+      networkCheckTimeout: 10000,
+      provider: () => {
+        return new HDWalletProvider(
+            secrets.ropsten.mnemonic,
+            `wss://ropsten.infura.io/ws/v3/${secrets.ropsten.projectId}`
+        );
+      },
+      network_id: "3",
     },
   },
   mocha: {
